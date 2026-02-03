52c006168446cd5cba13352e179ae68425c1b4f3
WilsonSalinasFlores <63874104+WilsonSalinasFlores@users.noreply.github.com>
2026-02-03 00:43:01 -0500

    remove mock

--- src/app/eventos/home/home.page.ts
+++ src/app/eventos/home/home.page.ts
@@ -88,7 +88,7 @@ export class HomePage implements OnInit {
     // Intentar cargar desde la API configurada en environment.apiUrl
     let desdeApi: Evento[] | null = null;
     try {
-      const useApiEvents=false;
+      const useApiEvents=true;
       if (environment?.apiUrl && useApiEvents) {
         desdeApi = await this.eventosSrv.cargarDesdeApiUrl();
       }
